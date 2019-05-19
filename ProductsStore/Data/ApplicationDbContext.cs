e863187bf382fddcc4b1bf3581428838585a2815
alenSavov <playmatte@gmail.com>
2019-05-19 11:11:42 +0300

    Deleted Bid from db

--- ProductsStore/Data/ApplicationDbContext.cs
+++ ProductsStore/Data/ApplicationDbContext.cs
@@ -14,7 +14,6 @@ namespace ProductsStore.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<SubCategory> SubCategories { get; set; }
         public DbSet<Item> Items { get; set; }
-        public DbSet<Bid> Bids { get; set; }
         public DbSet<Picture> Pictures { get; set; }
     }
 }
